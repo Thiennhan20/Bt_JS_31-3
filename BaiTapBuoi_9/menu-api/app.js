a88204121f8d32aead04eb997a901ce22ea75338
Thiện Nhân <152263908+Thiennhan20@users.noreply.github.com>
2025-03-31 21:32:36 +0700

    Add files via upload

--- BaiTapBuoi_9/menu-api/app.js
+++ BaiTapBuoi_9/menu-api/app.js
@@ -9,6 +9,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(express.static('public')); // Thêm dòng này
 
 // Routes
 app.use('/api/menus', menuRoutes);
@@ -16,5 +17,5 @@ app.use('/api/menus', menuRoutes);
 // Khởi động server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
